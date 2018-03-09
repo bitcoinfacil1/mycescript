472ac2168a55098b0eeda6ca2299408ba35d9236
Sunil Jimenez <sunil@bitcoinsfacil.com>
2018-03-09 12:27:40 -0300

    generating private key

--- myce_masternode_setup.sh
+++ myce_masternode_setup.sh
@@ -71,6 +71,8 @@ read PORT
 #echo ""
 #echo "Enter your masternode private key then press [ENTER]:"
 PRIVKEY=./myced genkey
+echo "This is you Private Key (copy and save it somewhere)"
+echo $PRIVKEY
 ./myced stop
 
 echo ""
@@ -100,7 +102,7 @@ echo "" >> $CONF_DIR/$CONF_FILE
 
 echo "" >> $CONF_DIR/$CONF_FILE
 echo "port=$PORT" >> $CONF_DIR/$CONF_FILE
-echo "masternodeaddress=$IP:$PORT" >> $CONF_DIR/$CONF_FILE
+echo "masternodeaddr=$IP:$PORT" >> $CONF_DIR/$CONF_FILE
 echo "masternodeprivkey=$PRIVKEY" >> $CONF_DIR/$CONF_FILE
 sudo ufw allow $PORT/tcp
 
